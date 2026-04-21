93f41c5bb08397faeb8dbc0defb3dce77a01399b
Titi <thedarktada@gmail.com>
2026-04-21 18:21:19 +0200

    fix: add lines in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,20 @@
 import pytest
 from app import greet, add, multiply
 
+
 def test_greet_normal():
     assert greet("Titi") == "Hello, Titi! Pipeline is alive 🚀"
+
 
 def test_greet_empty_raises():
     with pytest.raises(ValueError):
         greet("")
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
 
 def test_multiply():
     assert multiply(3, 4) == 12
